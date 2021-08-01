b41ca003542a9f325571974f87606328893bef3b
serhii.lomov <serhii.lomov23@gmail.com>
2021-08-01 14:57:13 +0300

    Initial Commit

---
+++ Statisticator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Statisticator
+//
+//  Created by Serhii.Lomov on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
